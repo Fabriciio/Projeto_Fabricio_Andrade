a096c16b949da6760e308c8eb1e3f4867cfa750d
Fabricio Andrade <fabricio.fonseca@estudante.ufla.br>
2019-09-16 18:41:11 -0300

    commit inicial

---
+++ classeA.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+int A1;
+float A2;
+
+
+void SetA1(int a) {
+	A1 = a;
+
+}
+
+int GetA1() {
+	return A1;
+}
+
+void MA1(){
+
+cout << "MA1";
+
+}
+void SetA2(int b) {
+	A2 = b;
+
+}
+
+int GetA2() {
+	return A2;
+}
+
+void MA2(){
+
+cout << "MA2";
+}
---
+++ classeB.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+int B1;
+float B2;
+
+
+void SetB1(int a) {
+	B1 = a;
+
+}
+
+int GetB1() {
+	return B1;
+}
+
+void MB1(){
+
+cout << "MB1";
+
+}
+void SetB2(int b) {
+	B2 = b;
+
+}
+
+int GetB2() {
+	return B2;
+}
+
+void MB2(){
+
+cout << "MB2";
+}
