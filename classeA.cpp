dfa0c1a3050b33ce682a74d835960264d3d3c075
Fabricio Andrade <fabricio.fonseca@estudante.ufla.br>
2019-09-16 20:42:57 -0300

    Criado Metodo MA3()

--- classeA.cpp
+++ classeA.cpp
@@ -10,13 +10,11 @@ float A2;
 
 void SetA1(int a) {
 	A1 = a;
-
 }
 
 int GetA1() {
 	return A1;
 }
-
 void MA1(){
 
 cout << "MA1";
@@ -24,7 +22,6 @@ cout << "MA1";
 }
 void SetA2(int b) {
 	A2 = b;
-
 }
 
 int GetA2() {
@@ -34,4 +31,9 @@ int GetA2() {
 void MA2(){
 
 cout << "MA2";
+}
+
+void MA3(){
+
+cout << "Alteração a classe A partir do clone";
 }
